a4751d0339e6388d1ce7ed96865cd7dce0c96aad
BerkayDur <Berkaydur138@gmail.com>
2024-06-11 12:48:37 +0100

    delete unneccary files

--- testing_workflows/main.py
+++
@@ -1,8 +0,0 @@
-'''This file contains a dummy function to test the github workflows for pytesting.'''
-
-def square(val: int) -> int:
-    '''square a number'''
-    return val ** 2
-
-if __name__ == '__main__':
-    ...
--- testing_workflows/test_main.py
+++
@@ -1,6 +0,0 @@
-from main import square
-import pytest
-
-@pytest.mark.parametrize('inp_out', [[1, 1], [2,3], [3, 9]])
-def test_square(inp_out):
-    assert square(inp_out[0]) == inp_out[1]
