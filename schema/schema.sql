2eb3acffb8aec7104ba01fe9349152ef0e3b6dcf
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-11 18:15:42 +0100

    yay seeding

--- schema/schema.sql
+++ schema/schema.sql
@@ -8,7 +8,7 @@ GO
 
 CREATE TABLE alpha.DIM_botanist (
     botanist_id INT IDENTITY(1, 1),
-    name TEXT NOT NULL,
+    name NVARCHAR(50),
     email TEXT,
     phone_no TEXT,
     PRIMARY KEY (botanist_id)
@@ -16,8 +16,8 @@ CREATE TABLE alpha.DIM_botanist (
 GO
 
 CREATE TABLE alpha.DIM_plant (
-    plant_id INT IDENTITY(1, 1),
-    plant_name TEXT NOT NULL,
+    plant_id INT,
+    plant_name TEXT,
     scientific_name TEXT,
     origin_longitude DECIMAL(9,6),
     origin_latitude DECIMAL(8,6),
@@ -30,14 +30,17 @@ GO
 
 CREATE TABLE alpha.FACT_plant_reading (
     reading_id INT IDENTITY(1, 1),
-    soil_moisture FLOAT NOT NULL,
-    temperature FLOAT NOT NULL,
-    last_watered datetime2 NOT NULL CHECK(last_watered < CURRENT_TIMESTAMP),
-    taken_at datetime2 NOT NULL,
-    plant_id INT NOT NULL,
-    botanist_id INT NOT NULL,
+    soil_moisture FLOAT,
+    temperature FLOAT,
+    last_watered datetime2 CHECK(last_watered < CURRENT_TIMESTAMP),
+    taken_at datetime2,
+    plant_id INT,
+    botanist_id INT,
     PRIMARY KEY (reading_id),
-    FOREIGN KEY (plant_id) REFERENCES alpha.DIM_plant(plant_id),
     FOREIGN KEY (botanist_id) REFERENCES alpha.DIM_botanist(botanist_id)
 );
 GO
+
+INSERT INTO alpha.DIM_plant(
+    
+)
