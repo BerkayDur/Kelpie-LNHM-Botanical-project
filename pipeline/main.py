a69942df87e4166065b45426b1fc199e8d04f9ae
joe1606 <josephlamkl75@outlook.com>
2024-06-12 17:20:05 +0100

    changed number of plants to env variable and fixed pylint score

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,15 +1,21 @@
+"""main"""
+
+from os import environ as ENV
+from dotenv import load_dotenv
 import extract
 import transform
 import load
 
-NUMBER_OF_PLANTS = 51
-
-
-def run_pipeline(number_of_plants: int = NUMBER_OF_PLANTS):
+def run_pipeline(number_of_plants: int):
+    """
+    Runs complete ETL pipeline
+    """
     api_data = extract.extract_data(number_of_plants)
     _, _, transformed_data = transform.transform_data(api_data)
     load.load_data(transformed_data)
 
 
 if __name__ == '__main__':
-    run_pipeline(NUMBER_OF_PLANTS)
+    load_dotenv()
+    num_plants = int(ENV["NUM_PLANTS"])
+    run_pipeline(num_plants)
