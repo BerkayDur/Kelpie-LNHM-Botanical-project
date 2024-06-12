7a2ab985c94edbaf70a8dbbbc263dcb0bb27ba67
BerkayDur <Berkaydur138@gmail.com>
2024-06-12 13:27:01 +0100

    add some tests and make some small changes to extract to add types

--- data_movement/extract.py
+++ data_movement/extract.py
@@ -13,8 +13,8 @@ import pandas as pd
 from dotenv import load_dotenv
 from pymssql import connect, Connection
 
+DEFAULT_TIME_FRAME_HOURS = 24
 
-HOURS = 24
 COLUMNS = ["reading_id", "soil_moisture", "temperature", "last_watered",
            "taken_at", "plant_id", "botanist_id"]
 
@@ -32,7 +32,9 @@ def get_connection() -> Connection:
 
 
 def get_the_time(number_of_hours: int) -> str:
-    """This function gets the time 24 hours ago as a formatted string."""
+    """This function gets the time number_of_hours hours ago as a formatted string."""
+    if not isinstance(number_of_hours, int) or number_of_hours < 0:
+        number_of_hours = DEFAULT_TIME_FRAME_HOURS
     now = datetime.now()
     yesterday = now - timedelta(hours=number_of_hours)
     yesterday_str = yesterday.strftime('%Y-%m-%d %H:%M:%S')
@@ -41,7 +43,7 @@ def get_the_time(number_of_hours: int) -> str:
 
 
 def query_and_extract_rds(time: str, conn: Connection) -> list[list]:
-    """This function creates the query to get the data from the last 24 hours"""
+    """This function creates the query to get the data start from time"""
     with conn.cursor() as cur:
         cur.execute(f"""
 SELECT *
@@ -55,6 +57,12 @@ WHERE taken_at >= '{time}'
 
 def load_into_parquet(data: list, cols: list[str]) -> None:
     """This function loads the given data into a parquet file"""
+    if not isinstance(data, list):
+        raise TypeError('Cannot insert data not of type list.')
+    if not isinstance(cols, list):
+        raise TypeError('cols must be of type list.')
+    if not all(isinstance(col, str) for col in cols):
+        raise TypeError('cols must contain a list of strings.')
     df = pd.DataFrame(data=data, columns=cols)
     df["plant_id"] = pd.to_numeric(
         df["plant_id"], errors='coerce').astype('Int64')
@@ -62,14 +70,14 @@ def load_into_parquet(data: list, cols: list[str]) -> None:
     df.to_parquet(ENV["FILE_NAME"], index=False)
 
 
-def extract_data() -> None:
+def extract_data(time_frame_hours: int) -> None:
     """
     Function that runs all the functions required to execute
     the goal of this task.
     """
     load_dotenv()
 
-    time_24_hours_ago = get_the_time(HOURS)
+    time_24_hours_ago = get_the_time(time_frame_hours)
 
     db_conn = get_connection()
     db_data = query_and_extract_rds(time_24_hours_ago, db_conn)
@@ -79,4 +87,4 @@ def extract_data() -> None:
 
 
 if __name__ == "__main__":
-    print(extract_data())
+    print(extract_data(DEFAULT_TIME_FRAME_HOURS))
