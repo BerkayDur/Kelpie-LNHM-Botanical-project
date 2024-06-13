8e32a68196eab6adaedd60a5600c9865866ca470
BerkayDur <Berkaydur138@gmail.com>
2024-06-13 09:50:47 +0100

    update extract_from_db to delete data read from the db

--- data_movement/extract_load_pipeline.py
+++ data_movement/extract_load_pipeline.py
@@ -5,9 +5,9 @@ from os import environ as CONFIG
 from dotenv import load_dotenv
 
 from load_to_s3 import load_to_s3
-from extract_from_db import extract_data
+from extract_from_db import extract_and_delete_data
 
 if __name__ == '__main__':
     load_dotenv('.env')
-    extract_data(CONFIG['TIME_FRAME_HOURS'])
+    extract_and_delete_data()
     load_to_s3(CONFIG)
