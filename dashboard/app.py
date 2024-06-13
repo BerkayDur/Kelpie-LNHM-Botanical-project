77042fe22375c383c39914c82ca2a67685a014bd
Alina <trainee.alina.tariq@sigmalabs.co.uk>
2024-06-13 14:00:57 +0100

    retrieving historical data from s3

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 from os import environ as ENV
 from dotenv import load_dotenv
 from boto3 import client
+import os
 import pandas as pd
 import streamlit as st
 import altair as alt
@@ -22,6 +23,39 @@ def get_connection():
         password=ENV["DB_PASSWORD"]
     )
     return conn
+
+
+def get_files(bucket_name, s3):
+    """gets all files from the specified bucket"""
+    return s3.list_objects(Bucket=bucket_name)
+
+
+def create_data_folder():
+    if not os.path.exists("./data"):
+        os.mkdir("./data")
+
+
+def get_most_recent(my_files: list):
+    last_modified = max([file["LastModified"]
+                        for file in my_files["Contents"]])
+    print(last_modified)
+    recent_file = [
+        file for file in my_files["Contents"] if file["LastModified"] == last_modified]
+    return recent_file[0]
+
+
+def get_bucket_data():
+    s3 = client('s3', aws_access_key_id=ENV["ACCESS_KEY"],
+                aws_secret_access_key=ENV["SECRET_ACCESS_KEY"])
+    create_data_folder()
+    files = get_files("c11-kelpie-lmnh-plant-data", s3)
+    recent_file = get_most_recent(files)
+    recent_key = recent_file["Key"]
+    s3.download_file("c11-kelpie-lmnh-plant-data", recent_key,
+                     os.path.join("data", "recent_data.parquet"))
+    df = pd.read_parquet(os.path.join("data", "recent_data.parquet"))
+    print(df)
+    return df
 
 
 def create_map(readings):
@@ -62,11 +96,19 @@ def create_latest_readings_bar(latest):
     ).interactive())
 
 
+def create_line_graph(data):
+    st.altair_chart(alt.Chart)
+
+
 if __name__ == "__main__":
     load_dotenv()
     conn = get_connection()
-    cursor = get_cursor(conn)
-    st.title("LMNH Plant Monitoring System 🌱")
+    # cursor = get_cursor(conn)
+    history = get_bucket_data()
+    print(history)
+    st.markdown("History")
+    history
+    st.title("🌿LMNH Plant Monitoring System 🌱")
     readings = pd.read_sql("""SELECT reading_id, soil_moisture, temperature, last_watered, taken_at, p.plant_id,
     plant_name, scientific_name, origin_longitude, origin_latitude, origin_town, origin_country_code, origin_region, b.botanist_id, name, email, phone_no
     FROM alpha.fact_plant_reading as pr
@@ -77,6 +119,7 @@ if __name__ == "__main__":
     JOIN alpha.dim_plant as p ON p.plant_id = pr.plant_id
     WHERE taken_at > DATEADD(minute, -1, (SELECT CURRENT_TIMESTAMP))
     ORDER BY plant_id;""", conn)
+    conn.close()
     # cursor.execute("""SELECT reading_id, soil_moisture, temperature, last_watered, taken_at, p.plant_id,
     # plant_name, scientific_name, origin_longitude, origin_latitude, origin_town, origin_country_code, origin_region, b.botanist_id, name, email, phone_no
     # FROM alpha.fact_plant_reading as pr
