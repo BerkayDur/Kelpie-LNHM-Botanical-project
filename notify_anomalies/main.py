5c657d2d83b9c5140e20cb101349a3ed32c34ca3
BerkayDur <Berkaydur138@gmail.com>
2024-06-14 11:41:47 +0100

    finish lambdarising

---
+++ notify_anomalies/main.py
@@ -0,0 +1,27 @@
+from os import environ as CONFIG
+from dotenv import load_dotenv
+
+from calculate_anomalies import calculate_anomalies
+from sns_alert import publish_to_topic
+
+def handler(event = None, context = None) -> dict:
+    '''Handler for lambda.'''
+    try:
+        load_dotenv()
+        message = calculate_anomalies()
+        if message != "No anomalies have been detected!":
+            publish_to_topic(CONFIG, Message=message)
+        return {
+            'status' : 'Success!!!!!!!!!'
+        }
+    except Exception as e:
+        return {
+            'status' : 'failed :(',
+            'reason' : str(e)
+        }
+    
+if __name__ == '__main__':
+    load_dotenv()
+    message = calculate_anomalies()
+    if message != "No anomalies have been detected!":
+        publish_to_topic(CONFIG, Message=message)
